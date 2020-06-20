a093960aa53cbe5c469a09b8d51b2bdebfea74a1
AnatoliiMikhov <anatoli.mihov@gmail.com>
2020-06-20 22:53:29 +0300

    import styled-components and usage

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,17 +6,31 @@ import PostStatusFilter from "../post-status-filter";
 import PostList from "../post-list";
 import PostAddForm from "../post-add-form";
 
-import "./app.css";
+// import "./app.css";
+import styled from "styled-components";
+
+const AppBlock = styled.div`
+	border-left: 1px solid #ccc;
+	border-right: 1px solid #ccc;
+	margin: 0 auto;
+	max-width: 1024px;
+	min-height: 100vh;
+	padding: 50px 20px 0;
+`;
+
+/* const AnotherAppBlock = styled(AppBlock)`
+	background-color: grey;
+`; */
 
 const App = () => {
 	const data = [
-		{label: "Going to learn React", important: true, id: "kkl"},
-		{label: "That is so good", important: false, id: "kdhj"},
-		{label: "I need a break from reality...", important: false, id: "jhfvj"},
+		{ label: "Going to learn React", important: true, id: "kkl" },
+		{ label: "That is so good", important: false, id: "kdhj" },
+		{ label: "I need a break from reality...", important: false, id: "jhfvj" },
 	];
 
 	return (
-		<div className="app">
+		<AppBlock>
 			<AppHeader />
 			<div className="search-panel d-flex">
 				<SearchPanel />
@@ -24,7 +38,7 @@ const App = () => {
 			</div>
 			<PostList posts={data} />
 			<PostAddForm />
-		</div>
+		</AppBlock>
 	);
 };
 
