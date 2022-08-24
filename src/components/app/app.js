86c3f1b77e4b8c064bde694b01b859f28379d6e3
Anatolii Mikhov <anatoli.mihov@gmail.com>
2022-08-24 16:28:54 +0300

    changes important star styles

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,152 +1,152 @@
-import React, {Component} from "react";
-import {uniqueID} from "../../generator/id-generator";
+import React, { Component } from "react"
+import { uniqueID } from "../../generator/id-generator"
 
-import AppHeader from "../app-header";
-import SearchPanel from "../search-panel";
-import PostStatusFilter from "../post-status-filter";
-import PostList from "../post-list";
-import PostAddForm from "../post-add-form";
+import AppHeader from "../app-header"
+import SearchPanel from "../search-panel"
+import PostStatusFilter from "../post-status-filter"
+import PostList from "../post-list"
+import PostAddForm from "../post-add-form"
 
-import "./app.css";
+import "./app.css"
 
 // import styled from "styled-components";
 /* const AppBlock = styled.div`
-	border-left: 1px solid #ccc;
-	border-right: 1px solid #ccc;
-	margin: 0 auto;
-	max-width: 1024px;
-	min-height: 100vh;
-	padding: 50px 20px 0;
+    border-left: 1px solid #ccc;
+    border-right: 1px solid #ccc;
+    margin: 0 auto;
+    max-width: 1024px;
+    min-height: 100vh;
+    padding: 50px 20px 0;
 `;
 
 const SearchPanelWrapper = styled.div`
-	display: flex;
-	margin: 1rem 0;
+    display: flex;
+    margin: 1rem 0;
 `; */
 
 export default class App extends Component {
-	// eslint-disable-next-line no-unused-vars
-	constructor(props) {
-		super();
-		this.state = {
-			data: [
-				{label: "Going to learn React", important: false, like: false, id: uniqueID()},
-				{label: "That is so good", important: false, like: false, id: uniqueID()},
-				{label: "I need a break from reality...", important: false, like: false, id: uniqueID()},
-			],
-			term: "",
-			filter: "all"
-		};
+    // eslint-disable-next-line no-unused-vars
+    constructor ( props ) {
+        super( props )
+        this.state = {
+            data: [
+                { label: "Going to learn React", important: false, like: false, id: uniqueID() },
+                { label: "That is so good", important: false, like: false, id: uniqueID() },
+                { label: "I need a break from reality...", important: false, like: false, id: uniqueID() },
+            ],
+            term: "",
+            filter: "all"
+        }
 
-		this.deleteItem = this.deleteItem.bind(this);
-		this.addItem = this.addItem.bind(this);
-		this.onToggleItemProperty = this.onToggleItemProperty.bind(this);
-		this.onUpdateSearch = this.onUpdateSearch.bind(this);
-		this.onFilterSelect = this.onFilterSelect.bind(this);
-	}
+        this.deleteItem = this.deleteItem.bind( this )
+        this.addItem = this.addItem.bind( this )
+        this.onToggleItemProperty = this.onToggleItemProperty.bind( this )
+        this.onUpdateSearch = this.onUpdateSearch.bind( this )
+        this.onFilterSelect = this.onFilterSelect.bind( this )
+    }
 
-	searchPost(items, term) {
-		if (term.length === 0) {
-			return items;
-		}
-		return items.filter((item) => {
-			return item.label.indexOf(term) > -1;
-		});
-	}
+    searchPost ( items, term ) {
+        if ( term.length === 0 ) {
+            return items
+        }
+        return items.filter( ( item ) => {
+            return item.label.indexOf( term ) > -1
+        } )
+    }
 
-	deleteItem(id) {
-		this.setState(({data}) => {
-			const index = data.findIndex(elem => elem.id === id);
-			const newData = [...data.slice(0, index), ...data.slice(index + 1)];
+    deleteItem ( id ) {
+        this.setState( ( { data } ) => {
+            const index = data.findIndex( elem => elem.id === id )
+            const newData = [ ...data.slice( 0, index ), ...data.slice( index + 1 ) ]
 
-			return {
-				data: newData
-			};
-		});
-	}
+            return {
+                data: newData
+            }
+        } )
+    }
 
-	addItem(body) {
-		const newItem = {
-			label: body,
-			important: false,
-			id: uniqueID()
-		};
-		this.setState(({data}) => {
-			const newData = [...data, newItem];
+    addItem ( body ) {
+        const newItem = {
+            label: body,
+            important: false,
+            id: uniqueID()
+        }
+        this.setState( ( { data } ) => {
+            const newData = [ ...data, newItem ]
 
-			return {
-				data: newData
-			};
-		});
-	}
+            return {
+                data: newData
+            }
+        } )
+    }
 
-	onToggleItemProperty(id, property) {
-		this.setState(({data}) => {
-			const index = data.findIndex(elem => elem.id === id);
+    onToggleItemProperty ( id, property ) {
+        this.setState( ( { data } ) => {
+            const index = data.findIndex( elem => elem.id === id )
 
-			const old = data[index];
-			const newItem = {...old};
-			newItem[property] = !old[property];
-			const before = data.slice(0, index);
-			const after = data.slice(index + 1);
-			const newArr = [...before, newItem, ...after];
+            const old = data[ index ]
+            const newItem = { ...old }
+            newItem[ property ] = !old[ property ]
+            const before = data.slice( 0, index )
+            const after = data.slice( index + 1 )
+            const newArr = [ ...before, newItem, ...after ]
 
-			return {
-				data: newArr
-			};
-		});
-	}
+            return {
+                data: newArr
+            }
+        } )
+    }
 
-	filterPost(items, filter) {
-		if (filter === "like") {
-			return items.filter(item => item.like);
-		} else {
-			return items;
-		}
-	}
+    filterPost ( items, filter ) {
+        if ( filter === "like" ) {
+            return items.filter( item => item.like )
+        } else {
+            return items
+        }
+    }
 
-	onUpdateSearch(term) {
-		this.setState({term});
-	}
+    onUpdateSearch ( term ) {
+        this.setState( { term } )
+    }
 
-	onFilterSelect(filter) {
-		this.setState({filter});
-	}
+    onFilterSelect ( filter ) {
+        this.setState( { filter } )
+    }
 
-	render() {
+    render () {
 
-		const {data, term, filter} = this.state;
-		const liked = data.filter(item => item.like).length;
-		const allPosts = data.length;
+        const { data, term, filter } = this.state
+        const liked = data.filter( item => item.like ).length
+        const allPosts = data.length
 
-		const visiblePost = this.filterPost(this.searchPost(data, term), filter);
+        const visiblePost = this.filterPost( this.searchPost( data, term ), filter )
 
-		return (
-			<div className="app">
+        return (
+            <div className="app">
 
-				<AppHeader
-					liked={liked}
-					allPosts={allPosts}
-				/>
+                <AppHeader
+                    liked={ liked }
+                    allPosts={ allPosts }
+                />
 
-				<div className="search-panel d-flex">
-					<SearchPanel
-						onUpdateSearch={this.onUpdateSearch}
-					/>
-					<PostStatusFilter
-						filter={filter}
-						onFilterSelect={this.onFilterSelect}
-					/>
-				</div>
+                <div className="search-panel d-flex">
+                    <SearchPanel
+                        onUpdateSearch={ this.onUpdateSearch }
+                    />
+                    <PostStatusFilter
+                        filter={ filter }
+                        onFilterSelect={ this.onFilterSelect }
+                    />
+                </div>
 
-				<PostList posts={visiblePost}
-					onDelete={this.deleteItem}
-					onToggleItemProperty={this.onToggleItemProperty}
-				/>
+                <PostList posts={ visiblePost }
+                    onDelete={ this.deleteItem }
+                    onToggleItemProperty={ this.onToggleItemProperty }
+                />
 
-				<PostAddForm onAdd={this.addItem} />
+                <PostAddForm onAdd={ this.addItem } />
 
-			</div>
-		);
-	}
+            </div>
+        )
+    }
 }
--- src/components/post-list-item/post-list-item.js
+++ src/components/post-list-item/post-list-item.js
@@ -1,52 +1,53 @@
 /* eslint-disable react/prop-types */
-import React from "react";
+import React from "react"
 
-import "./post-list-item.scss";
+import "./post-list-item.scss"
 
 export default class PostListItem extends React.Component {
 
-	render() {
-		const {label, onDelete, onToggleImportant, onToggleLiked, important, like} = this.props; // TODO fix eslint prop-types
+    render () {
+        const { label, onDelete, onToggleImportant, onToggleLiked, important, like } = this.props // TODO fix eslint prop-types
 
-		let classNames = "app-list-item d-flex justify-content-between";
+        let classNames = "app-list-item d-flex justify-content-between"
 
-		if (important) {
-			classNames += " important";
-		}
+        if ( important ) {
+            classNames += " important"
+        }
 
-		if (like) {
-			classNames += " like";
-		}
+        if ( like ) {
+            classNames += " like"
+        }
 
-		return (
-			<div className={classNames}>
-				<span
-					className="app-list-item-label w-100"
-					onClick={onToggleLiked}
-				>
-					{label}
-				</span>
+        return (
+            <div className={ classNames }>
+                <span
+                    className="app-list-item-label w-100"
+                    title='To like'
+                    onClick={ onToggleLiked }
+                >
+                    { label }
+                </span>
 
-				<div className="d-flex justify-content-center align-items-center">
-					<button
-						type="button"
-						className="btn-star btn-sm"
-						onClick={onToggleImportant}
-					>
-						<i className="fa fa-star"></i>
-					</button>
+                <div className="d-flex justify-content-center align-items-center">
+                    <button
+                        type="button"
+                        className="btn-star btn-sm"
+                        onClick={ onToggleImportant }
+                    >
+                        <i className="fa fa-star"></i>
+                    </button>
 
-					<button
-						type="button"
-						className="btn-trash btn-sm"
-						onClick={onDelete}
-					>
-						<i className="fa fa-trash-o"></i>
-					</button>
+                    <button
+                        type="button"
+                        className="btn-trash btn-sm"
+                        onClick={ onDelete }
+                    >
+                        <i className="fa fa-trash-o"></i>
+                    </button>
 
-					<i className="fa fa-heart"></i>
-				</div>
-			</div>
-		);
-	}
+                    <i className="fa fa-heart"></i>
+                </div>
+            </div>
+        )
+    }
 }
